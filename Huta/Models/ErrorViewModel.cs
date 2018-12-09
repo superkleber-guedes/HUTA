cf72868ceafd35a04c3642e4f9248e2167ff4504
Kleber Guedes <Kleber.guedes@runpath.com>
2018-12-09 17:18:55 +0000

    First commit

---
+++ Huta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Huta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
