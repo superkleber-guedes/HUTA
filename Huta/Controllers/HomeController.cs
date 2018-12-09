7ded44236d456ec58bce06cc40e55d0d6506d22f
Kleber Guedes <Kleber.guedes@runpath.com>
2018-12-09 17:45:26 +0000

    testing deploy

--- Huta/Controllers/HomeController.cs
+++ Huta/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Huta.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Hello world. Your application description page.";
 
             return View();
         }
