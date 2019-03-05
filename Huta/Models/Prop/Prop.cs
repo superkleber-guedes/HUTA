de9db69c012839f2ab414ad174844339368bde04
Kleber Guedes <Kleber.guedes@runpath.com>
2019-03-05 17:33:57 +0000

    using json instead (part 1)

---
+++ Huta/Models/Prop/Prop.cs
@@ -0,0 +1,71 @@
+﻿namespace Huta.Prop
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Prop
+    {
+        [JsonProperty("intro")]
+        public List<BeforeAndAfterData> Intro { get; set; }
+
+        [JsonProperty("daily")]
+        public List<DailyMessage> Daily { get; set; }
+
+        [JsonProperty("end")]
+        public List<BeforeAndAfterData> End { get; set; }
+    }
+
+    public partial class DailyMessage
+    {
+        [JsonProperty("Date")]
+        public DateTimeOffset Date { get; set; }
+
+        [JsonProperty("Message")]
+        public string Message { get; set; }
+
+        [JsonProperty("SubMessage", NullValueHandling = NullValueHandling.Ignore)]
+        public string SubMessage { get; set; }
+
+        [JsonProperty("ImagePath", NullValueHandling = NullValueHandling.Ignore)]
+        public string ImagePath { get; set; }
+    }
+
+    public partial class BeforeAndAfterData
+    {
+        [JsonProperty("order")]
+        public long Order { get; set; }
+
+        [JsonProperty("Message")]
+        public string Message { get; set; }
+
+        [JsonProperty("SubMessage", NullValueHandling = NullValueHandling.Ignore)]
+        public string SubMessage { get; set; }
+    }
+
+    public partial class Prop
+    {
+        public static Prop FromJson(string json) => JsonConvert.DeserializeObject<Prop>(json, Huta.Prop.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Prop self) => JsonConvert.SerializeObject(self, Huta.Prop.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
